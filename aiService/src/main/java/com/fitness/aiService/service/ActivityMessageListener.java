563ddbcc8d35b496d415e40db7abcfcfe1431b79
Chriscoded <amaemechris@gmail.com>
2026-02-02 11:40:27 +0100

    listen to rabbitmq messages

---
+++ aiService/src/main/java/com/fitness/aiService/service/ActivityMessageListener.java
@@ -0,0 +1,28 @@
+package com.fitness.aiService.service;
+
+import com.fitness.aiService.model.Activity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Service
+//@Slf4j
+@RequiredArgsConstructor
+public class ActivityMessageListener {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(ActivityMessageListener.class);
+
+    @Value("${rabbitmq.queue.name}")
+    private  String queueNamw;
+
+    @RabbitListener(queues = "activity.queue")
+    public void processActivity(Activity activity){
+        log.info("$ Received Activity for precessing {}", activity.getId());
+    }
+}
